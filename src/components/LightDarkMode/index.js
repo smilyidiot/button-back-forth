b2049ff3206eecc8ec21d55558f2a45fe81ef6e6
smilyidiot <vinayakrachha@gmail.com>
2022-10-08 18:17:48 +0000

    changing background with button using render

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,35 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+  state: {status: true}
+
+  onClickButton = () => {
+    const {status} = this.state
+    this.setState({status: !status})
+  }
+
+  render() {
+    const {status} = this.state
+    const mode = status ? 'blackCard' : 'lightCard'
+    const buttonMode = status ? 'darkCardBton' : 'lightCardBton'
+    const buttonText = status ? 'Light Mode' : 'Dark Mode'
+    return (
+      <div className="bg-cont">
+        <div className={mode}>
+          <h1>Click To Change Mode</h1>
+          <button
+            type="button"
+            className={buttonMode}
+            // onClick={this.onClickButton}
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
